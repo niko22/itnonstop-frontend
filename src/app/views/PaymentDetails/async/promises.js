8c259236862a83c5af9649e7adcfa1ab56240cca
Bartosz Matosiuk <bartosz.matosiuk@gmail.com>
2017-11-22 09:47:31 +0100

    Adding async styles structure

---
+++ src/app/views/PaymentDetails/async/promises.js
@@ -0,0 +1,3 @@
+export default function getDataWithPromises(paymentId) {
+  console.log(`Loading data for ${paymentId} using promises`);
+}
--- src/app/views/PaymentDetails/index.jsx
+++ src/app/views/PaymentDetails/index.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import getDataWithCallbacks from './async/callbacks';
+import getDataWithPromises from './async/promises';
+import getDataWithAsyncAwait from './async/async-await';
 
 export default class PaymentDetails extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       progress: 0,
       progressLabel: 'Loading',
       payments: [],
     };
+  }
+
+  componentWillMount() {
+    const {paymentId, asyncStyle} = this.props.params;
+    if (asyncStyle === 'callbacks') {
+      getDataWithCallbacks(paymentId);
+    } else if (asyncStyle === 'promises') {
+      getDataWithPromises(paymentId);
+    } else {
+      getDataWithAsyncAwait(paymentId);
+    }
   }
 
   render() {
